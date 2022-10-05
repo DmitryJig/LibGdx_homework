fc32578f432bdfa9d2c03e70fbd869dc5050104a
dima <jd2344430@gmail.com>
2022-10-05 12:43:55 +0500

    homework_2

--- core/src/com/mygdx/game/GDX_Game_homework.java
+++ core/src/com/mygdx/game/GDX_Game_homework.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /**
@@ -16,15 +19,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
  */
 public class GDX_Game_homework extends ApplicationAdapter {
     private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
     private MyAtlasAnim walk, stand, tmpA;
     private Music music;
     private Sound sound;
     private MyInputProcessor myInputProcessor;
     private float x, y;
     int dir = 0, step = 1;
+    private Rectangle rectangle, window;
 
     @Override
     public void create() {
+
+        shapeRenderer = new ShapeRenderer();
+        rectangle = new Rectangle();
+        window = new Rectangle(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         myInputProcessor = new MyInputProcessor();
         Gdx.input.setInputProcessor(myInputProcessor);
@@ -79,11 +88,27 @@ public class GDX_Game_homework extends ApplicationAdapter {
         if (!tmpA.draw().isFlipX() && dir == -1) tmpA.draw().flip(true, false);
         if (tmpA.draw().isFlipX() && dir == 1) tmpA.draw().flip(true, false);
 
+        rectangle.x = x;
+        rectangle.y = y;
+        rectangle.width = tmp.getRegionWidth();
+        rectangle.height = tmp.getRegionHeight();
+
         tmpA.setTime(Gdx.graphics.getDeltaTime());
 
         batch.begin();
         batch.draw(tmpA.draw(), x, y);
         batch.end();
+
+        // отрисовываем квадратик вокруг персонажа
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        shapeRenderer.end();
+        if (!window.overlaps(rectangle)) { // если все тело вышло за пределы границы окна
+            Gdx.graphics.setTitle("Out");
+        }
+//        if (!window.contains(rectangle)) Gdx.graphics.setTitle("Out"); // если выступили за границу окна
+        else Gdx.graphics.setTitle("In");
     }
 
     @Override
@@ -93,5 +118,6 @@ public class GDX_Game_homework extends ApplicationAdapter {
         stand.dispose();
         tmpA.dispose();
         music.dispose();
+        shapeRenderer.dispose();
     }
 }
